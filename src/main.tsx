e93b4e1632b4951439065d504f1b3fbab2866a86
Jhan Renz <jhanrenzventura@gmail.com>
2025-05-12 19:19:21 +0800

    yt

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import Youtube from "./components/Youtube/Youtube";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-   <App />
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4"></h1>
+      <Youtube videoId="dQw4w9WgXcQ" /> {/* Replace with any video ID */}
+    </div>
   </StrictMode>
 );
